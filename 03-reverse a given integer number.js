494ea7a41dda704574716f3ae894608795b5ccf1
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2022-09-20 23:49:18 +0530

    Create 03-reverse a given integer number.js

---
+++ 03-reverse a given integer number.js
@@ -0,0 +1,12 @@
+/*
+Write a program to reverse a given integer number
+The remainder of the number can be fetched and the number can be divided by 10 to remove the the digit in loop till number becomes 0
+A simple approach to reverse a number could also be to convert it in to a string and then reverse it
+*/
+const num = 3849;
+
+function reverseGivenInteger(num) {
+    return parseInt(num.toString().split('').reverse().join(''));
+}
+
+console.log(`Reversed integer is: ${reverseGivenInteger(num)}`)
