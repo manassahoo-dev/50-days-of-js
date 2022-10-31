d53326ccc9048c86388363a317280b27a82c87ef
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2022-10-31 23:26:13 +0530

    Create 43-Highest Scoring Word.js

---
+++ 43-Highest Scoring Word.js
@@ -0,0 +1,25 @@
+/*
+Highest Scoring Word
+Given a string of words, you need to find the highest scoring word.
+
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid.
+*/
+
+function high(x){
+const str = x.split(' ');
+const result1 = [];
+const result = str.reduce((_, dig) => {
+  let c = 0;
+  for (let j = 0; j < dig.length; j++) {
+    c = c + (dig.charCodeAt(j) - 96);
+  }
+  result1.push(c);
+}, 0);
+return str[result1.indexOf(result1.slice().sort((a, b) => b - a)[0])];
+}
